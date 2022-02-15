15618d74fa916ee7dd6d251d1994172afe5ee801
bene111 <77780337+bene111@users.noreply.github.com>
2022-02-15 18:34:22 +0800

    Update cl_reply.js

--- cl_reply.js
+++ cl_reply.js
@@ -24,6 +24,7 @@ if (process.env.UA) {
 }
 reply_news_arr = ['1024','感谢分享','感谢你的分享','谢谢分享','多谢分享','感谢作者分享','谢谢坛友分享','感谢分享','支持分享','支持发帖','感谢发帖']
 let clreplycount = (process.env.CLREPLYCOUNT) ? process.env.CLREPLYCOUNT : 10
+console.log('CLREPLYCOUNT值为：' + clreplycount)
 !(async () => {
     if (!clcookiesArr[0]) {
         $.msg($.name, '请先添加cookie');
