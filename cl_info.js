cd192207e71bf55af009475ada00e298ba615066
bene111 <77780337+bene111@users.noreply.github.com>
2022-03-04 19:57:26 +0800

    Update cl_info.js

--- cl_info.js
+++ cl_info.js
@@ -285,7 +285,7 @@ async function getbankinfo() {
                         let sjdqsjdt = new Date(sjdqsj.replace("-","/")); //定期到期时间
 
                         console.log('当前时间：' + time, '\n定期到期时间：' + sjdqsjdt)
-                        if (time < sjdqsjdt) {
+                        if (time > sjdqsjdt) {
                             //ismessage = true
                             await tgBotNotify($.name, `用户${$.index}：${username}\n定期存款到期时间：${sjdqsjdt}\n当前时间：${time}\n已到期，请及时处理`, '', `\n`)
                             message += `当前时间为：${time}\n当前账号定期存款已到期\n\n`  
